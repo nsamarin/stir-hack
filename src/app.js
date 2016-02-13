15c0e1f5a7b6daefcb9d921b653afb8a186690b5
Nikita Samarin <nsamarin@users.noreply.github.com>
2016-02-13 14:25:39 +0000

    Add simple app layout

--- src/app.js
+++ src/app.js
@@ -1,64 +1,20 @@
 /**
- * Welcome to Pebble.js!
+ * 
  *
- * This is where you write your app.
+ * 
  */
 
 var UI = require('ui');
-var Vector2 = require('vector2');
 
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.',
-  subtitleColor: 'indigo', // Named colors
-  bodyColor: '#9a0036' // Hex colors
+// Create a Card with title and subtitle
+var card = new UI.Card({
+  title:'PebbleRecognition',
+  subtitle:'Enter the right button combination'
 });
 
-main.show();
-
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
+card.on('click', 'up', function() {
+  console.log('Up clicked!');
 });
 
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    fullscreen: true,
-  });
-  var textfield = new UI.Text({
-    position: new Vector2(0, 65),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
-
-// Hello
+// Display the Card
+card.show();
