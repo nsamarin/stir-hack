99799101574f137a9b83bda9eb001a6454707515
Nikita Samarin <nsamarin@users.noreply.github.com>
2016-02-13 16:47:02 +0000

    Update app with recieveData and update auth.js

--- src/app.js
+++ src/app.js
@@ -7,19 +7,31 @@
 var UI = require('ui');
 var auth = require('./authenticate');
 
-auth.main();
+//auth.main();
 
 
+ // Create a Card with title and subtitle
+ var card = new UI.Card({
+   title:'Test Card',
+   subtitle:'Enter the right button combination'
+ });
 
-// // Create a Card with title and subtitle
-// var card = new UI.Card({
-//   title:'JP Morgan',
-//   subtitle:'Enter the right button combination'
-// });
+ card.on('click', 'up', function() {
+   console.log('Up clicked!');
+   
+   var transactionId = Pebble.sendAppMessage( { '0': 42, '1': 'String value' },
+       function(e) {
+           console.log('Successfully delivered message with transactionId='
+                   + e.data.transactionId);
+       },
+       function(e) {
+           console.log('Unable to deliver message with transactionId='
+                   + e.data.transactionId
+                   + ' Error is: ' + e.error.message);
+       }
+       );
+   
+ });
 
-// card.on('click', 'up', function() {
-//   console.log('Up clicked!');
-// });
-
-// // Display the Card
-// card.show();
+ // Display the Card
+ card.show();
