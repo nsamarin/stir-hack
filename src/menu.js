573fe5d3f4f80de3e52c7aa8ca8da5954d7afa40
Nikita Samarin <nsamarin@users.noreply.github.com>
2016-02-14 00:12:07 +0000

    Merge branches

--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,8 @@
 var UI = require('ui');
-var accounts = require('./accounts');
 var transactions = require('./viewTransactions');
+var accounts = require('./accounts');
+var contacts = require('./payment');
+
 // Construct Menu to show to user
 
 var mainAccSum = 5000;
@@ -34,9 +36,20 @@ var resultsMenu = new UI.Menu({
   }]
 });
 
-// Show the Menu, hide the splash
+
+// Show the Menu
 exports.main = function(){
   resultsMenu.show();
+  
+  // Register for 'tap' events
+  resultsMenu.on('accelTap', function(e) {
+    console.log('TAP!');
+    console.log('axis: ' + e.axis + ', direction:' + e.direction);
+    resultsMenu.items(0,   {
+      title: "150£",
+      subtitle: mainAccNum.toString()
+    });
+  });
   
   // Add an action for menu options
   resultsMenu.on('select', function(e) {
@@ -44,10 +57,8 @@ exports.main = function(){
     if (e.itemIndex === 0) accounts.main();
     if (e.itemIndex == 1) {}
     if (e.itemIndex == 2) transactions.main();
-    if (e.itemIndex == 3) {}
-    
+    if (e.itemIndex == 3) contacts.main();
     
     console.log('Item number ' + e.itemIndex + ' was pressed!');
   });
 };
-//splashWindow.hide();
